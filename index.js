6df3e32a31b388f3ce83be4f7005c938a4f5a9c1
Taylor Ng <taylormng95@gmail.com>
2024-05-14 17:04:57 -0400

    completes array lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+const cats = ['Milo','Otis','Garfield'];
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+    const newCatArray = [...cats, name];
+    return newCatArray;
+}
+
+function prependCat(name){
+    const newCatArray = [name, ...cats];
+    return newCatArray;
+}
+
+function removeLastCat(){
+    const newCatArray = [...cats].slice(0,cats.length-1);
+    return newCatArray;
+}
+
+function removeFirstCat(){
+    const newCatArray = [...cats].slice(1,cats.length)
+    return newCatArray;
+}
